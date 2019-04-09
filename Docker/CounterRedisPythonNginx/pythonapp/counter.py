e4ef94daef4a33e5a5e199bb4e49385432059f98
aleskandro <aleskandro@seraph>
2019-04-09 11:57:28 +0200

    First commit

---
+++ Docker/CounterRedisPythonNginx/pythonapp/counter.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import redis
+from fix_nginx import ReverseProxied
+app = Flask(__name__)
+#app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+redis = redis.Redis(host="redis")
+
+@app.route("/backend/")
+def count():
+    redis.incr('counter')
+    return "Count is %s" % redis.get('counter').decode("utf-8")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
