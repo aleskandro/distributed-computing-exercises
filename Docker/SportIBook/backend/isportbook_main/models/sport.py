64b97578bc6c9e4911be4820d217196736c09f76
aleskandro <aleskandro@seraph>
2019-11-28 15:00:45 +0100

    Deleted SportIBook Docker example

--- Docker/SportIBook/backend/isportbook_main/models/sport.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-class Sport(models.Model):
-    name = models.CharField(max_length=100, blank=False, verbose_name=_("Name"))
-    icon = models.ImageField(upload_to="sports/", null=True, verbose_name=_("Icon"))
-
-    def __str__(self):
-        return self.name
--- Docker/SportIBook/backend/isportbook_main/serializers/city.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from isportbook_main.models.city import City
-
-class CitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = City
-        fields = ("name","id")
-
